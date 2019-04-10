31cacbf60822861bc806b00fd3082ba9e24e1647
jacob-ressler <jressler96@gmail.com>
2019-04-10 01:32:16 -0400

    Added method to get all branches, Added private field to store all branches

--- src/data/FractalTree.java
+++ src/data/FractalTree.java
@@ -1,20 +1,47 @@
 package data;
 
 import java.awt.Graphics2D;
+import java.util.ArrayList;
+
+import utilities.Debug;
 
 // Data structure for the fractal tree
 public class FractalTree {
 	
-	private Branch trunk; // reference to initial branch (equivalent to root of a typical tree data structure)
-	
+	private Branch root; // reference to initial branch (equivalent to root of a typical tree data structure)
+	private Branch[] branches; // all branches of the tree (including root)
 	
 	public FractalTree() {
-		this.trunk = new Branch(Parameters.tilt);
+		this.root = new Branch(Parameters.tilt);
 	}
 
 	
 	public void draw(Graphics2D g, int height) {
-		trunk.draw(g, height);
-		//System.out.println("Tree drawn");
+		root.draw(g, height);
+	}
+	
+	// get all branches in this tree
+	public Branch[] getAllBranches() {
+		if (branches != null) return branches; // this has all the branches
+		
+		branches = (Branch[]) getAllBranches(root, new ArrayList<Branch>()).toArray();
+		
+		Debug.printArray(branches);
+		
+		return branches;
+	}
+	
+	// recursively add all branches to the list
+	private ArrayList<Branch> getAllBranches(Branch curr, ArrayList<Branch> list) {
+		list.add(curr); // add this to the list
+		
+		if (curr.getChildren() == null)
+			return list;
+		
+		for (int i = 0; i < curr.getChildren().length; i++) {
+			getAllBranches(curr.getChildren()[i], list);
+		}
+		
+		return list;
 	}
 }
