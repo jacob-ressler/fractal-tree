0589e2250eff454cf45fa294f1a932bf86805927
Jacob Ressler <jressler96@gmail.com>
2019-04-14 01:08:02 -0400

    Removed draw(), added getBranchesUpToGen()
    All draw functionality is now handled in TreeCanvas. The getBranchesUpToGen() method returns an array of all branches up to the specified generation.

--- src/data/FractalTree.java
+++ src/data/FractalTree.java
@@ -1,47 +1,59 @@
 package data;
 
-import java.awt.Graphics2D;
 import java.util.ArrayList;
-
-import utilities.Debug;
 
 // Data structure for the fractal tree
 public class FractalTree {
-	
-	private Branch root; // reference to initial branch (equivalent to root of a typical tree data structure)
-	private Branch[] branches; // all branches of the tree (including root)
-	
+
+	private Branch root; // reference to initial branch (equivalent to root of a typical tree data
+							// structure)
+	private ArrayList<Branch> branches; // all branches of the tree (including root)
+
 	public FractalTree() {
 		this.root = new Branch(Parameters.tilt);
 	}
 
-	
-	public void draw(Graphics2D g, int height) {
-		root.draw(g, height);
-	}
-	
+//	public void draw(Graphics2D g, int height) {
+//		root.draw(g, height);
+//	}
+
 	// get all branches in this tree
 	public Branch[] getAllBranches() {
-		if (branches != null) return branches; // this has all the branches
-		
-		branches = (Branch[]) getAllBranches(root, new ArrayList<Branch>()).toArray();
-		
-		Debug.printArray(branches);
-		
-		return branches;
-	}
-	
-	// recursively add all branches to the list
-	private ArrayList<Branch> getAllBranches(Branch curr, ArrayList<Branch> list) {
-		list.add(curr); // add this to the list
-		
-		if (curr.getChildren() == null)
-			return list;
-		
-		for (int i = 0; i < curr.getChildren().length; i++) {
-			getAllBranches(curr.getChildren()[i], list);
+		if (branches == null) {
+			branches = new ArrayList<Branch>();
+			getAllBranches(root);
+
+			//Debug.logArray((Branch[]) branches.toArray());
 		}
 		
-		return list;
+		return branches.toArray(new Branch[1]);
+	}
+
+	// recursively add all branches to the list
+	private void getAllBranches(Branch curr) {
+		branches.add(curr); // add this to the list
+
+		if (curr.getChildren() == null)
+			return;
+
+		for (int i = 0; i < curr.getChildren().length; i++) {
+			getAllBranches(curr.getChildren()[i]);
+		}
+
+	}
+
+	public Branch[] getBranchesUpToGen(int gen) {
+		if (branches == null)
+			getAllBranches();
+
+		ArrayList<Branch> bs = new ArrayList<Branch>();
+		for (Branch b : branches) {
+			if (b.getGeneration() <= gen) {
+				//Debug.log(b.toString());
+				bs.add(b);
+			}
+		}
+
+		return bs.toArray(new Branch[1]);
 	}
 }
