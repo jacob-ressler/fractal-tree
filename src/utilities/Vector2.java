6788b18f999f9d17c8bc0c75ef045ab7e1681c77
Jacob Ressler <jressler96@gmail.com>
2019-04-14 01:20:16 -0400

    Added static zero() to create a zero vector
    Calling Vector2.zero() will return a new Vector2 with x = 0 and y = 0.

--- src/utilities/Vector2.java
+++ src/utilities/Vector2.java
@@ -28,6 +28,10 @@ public class Vector2 {
 		return rot;
 	}
 
+	public static Vector2 zero() {
+		return new Vector2(0, 0);
+	}
+	
 	// return Euclidean distance as an int
 	public static int distance(Vector2 start, Vector2 end) {
 		int xdif = (start.x - end.x) * (start.x - end.x);
