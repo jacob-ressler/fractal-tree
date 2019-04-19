0457c0317803e7586da2bd2a0247cd8ebf8f6153
Jacob Ressler <jressler96@gmail.com>
2019-04-19 15:23:38 -0400

    Renamed ParamPanel to CustomizationPanel.
    Also changed its constructor to create a SingleValueTab, RangedValueTab, and MiscTab instead of the filler JPanels.

---
+++ src/gui/CustomizationPanel.java
@@ -0,0 +1,45 @@
+package gui;
+
+import java.awt.Dimension;
+
+import javax.swing.JTabbedPane;
+
+import gui.tabs.MiscTab;
+import gui.tabs.RangedValueTab;
+import gui.tabs.SingleValueTab;
+import gui.tabs.Tab;
+
+public class CustomizationPanel extends JTabbedPane {
+
+	Tab svt, rvt, misc;
+	
+	public CustomizationPanel(int w, int h) {
+		
+		setPreferredSize(new Dimension(w, h));
+		setMinimumSize(new Dimension(250, 250));
+		
+		//create 3 tabs
+		svt = new SingleValueTab();
+		addTab("Single", null, svt, "Use single values for all parameters");
+		
+		rvt = new RangedValueTab();
+		addTab("Ranged", null, rvt, "Use ranged values for all parameters");
+		
+		misc = new MiscTab();
+		addTab("Misc.", null, misc, "Set color pattern, toggle animation");
+		
+	}
+
+	
+//	protected JComponent makeTextPanel(String text) {
+//		JPanel panel = new JPanel(false);
+//		JLabel filler = new JLabel(text);
+//		filler.setHorizontalAlignment(JLabel.CENTER);
+//		panel.setLayout(new GridLayout(1,1));
+//		panel.add(filler);
+//		return panel;
+//	}
+	
+	
+		
+}
