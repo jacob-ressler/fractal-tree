9e6e0ec961cfc33e0e33bf671cdea795afc1be91
Jacob Ressler <jressler96@gmail.com>
2019-04-24 23:08:00 -0400

    tried to set char limit with DocumentFilter
    in theory it should work... but in practice it has not as of yet.

--- src/utilities/JTextFieldLimit.java
+++ src/utilities/JTextFieldLimit.java
@@ -2,6 +2,7 @@ package utilities;
 
 import javax.print.attribute.AttributeSet;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
 import javax.swing.text.PlainDocument;
 
 public class JTextFieldLimit extends PlainDocument {
@@ -9,6 +10,30 @@ public class JTextFieldLimit extends PlainDocument {
 
    public JTextFieldLimit(int limit) {
    super();
+   setDocumentFilter(new DocumentFilter() {
+
+		@Override
+		public void insertString(FilterBypass fb, int offset, String string, javax.swing.text.AttributeSet attr)
+				throws BadLocationException {
+			if (fb.getDocument().getLength() + string.length() > limit) {
+				return;
+			}
+			
+			super.insertString(fb, offset, string, attr);
+		}
+	
+		@Override
+		public void remove(FilterBypass fb, int offset, int length) throws BadLocationException {
+			super.remove(fb, offset, length);
+		}
+	
+		@Override
+		public void replace(FilterBypass fb, int offset, int length, String text, javax.swing.text.AttributeSet attrs)
+				throws BadLocationException {
+			super.replace(fb, offset, length, text, attrs);
+		}
+	   
+   });
    this.limit = limit;
    }
 
